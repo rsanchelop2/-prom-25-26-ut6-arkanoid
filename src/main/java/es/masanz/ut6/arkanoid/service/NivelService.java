172bc961208d1c16821bbe3f64e7e6abfe9ccce1
Raul Sanchez <rsanchelop2@educacion.navarra.es>
2026-03-02 12:50:41 +0100

    TODO 01 Y 02 Completados

--- src/main/java/es/masanz/ut6/arkanoid/service/NivelService.java
+++ src/main/java/es/masanz/ut6/arkanoid/service/NivelService.java
@@ -6,7 +6,8 @@ import es.masanz.ut6.arkanoid.model.Nivel;
 public class NivelService {
 
     public static Nivel obtenerNivel(int id) {
-        // TODO 02: Buscar y devolver el nivel en caso de existir
+        // TODO 02: Buscar y devolver el nivel en caso de existir - DONE (CREO)
+        if (NivelDao.obtenerNivel(id) != null) return NivelDao.obtenerNivel(id);
         return null;
     }
 }
