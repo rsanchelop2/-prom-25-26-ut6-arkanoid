ef04de50f06d2f5cfbcc8697fb55fb5d4d0bece6
Raul Sanchez <rsanchelop2@educacion.navarra.es>
2026-03-02 12:45:23 +0100

    TODO 02 Completado

--- src/main/java/es/masanz/ut6/arkanoid/dao/NivelDao.java
+++ src/main/java/es/masanz/ut6/arkanoid/dao/NivelDao.java
@@ -1,12 +1,29 @@
 package es.masanz.ut6.arkanoid.dao;
 
 import es.masanz.ut6.arkanoid.database.ConnectionManager;
+import es.masanz.ut6.arkanoid.model.Ladrillo;
 import es.masanz.ut6.arkanoid.model.Nivel;
+
+import java.util.List;
 
 public class NivelDao {
 
     public static Nivel obtenerNivel(int id){
-        // TODO 01: Buscar y devolver el nivel en caso de existir
+        // TODO 01: Buscar y devolver el nivel en caso de existir - DONE
+
+        String sql = "SELECT id, filas, columnas, ladrillos, siguiente_nivel FROM nivel WHERE id = ?";
+        Object[] params = {};
+        Object[][] resul = ConnectionManager.ejecutarSelectSQL(sql, params);
+        if (resul != null && resul.length > 0){
+            Nivel nivel = new Nivel();
+            nivel.setId((Integer) resul[0][1]);
+            nivel.setFilas((Integer) resul[0][2]);
+            nivel.setColumnas((Integer) resul[0][3]);
+            nivel.setLadrillos((List<Ladrillo>) resul[0][4]);
+            nivel.setSiguienteNivel((Integer) resul[0][5]);
+
+            return nivel;
+        }
         return null;
     }
 
